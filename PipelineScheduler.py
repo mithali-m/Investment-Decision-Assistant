d69345ae6a4ddcc6610ca3e0101fcb37e465de76
Mithali Manjunath <70499394+mithali-m@users.noreply.github.com>
2024-12-13 13:17:23 -0500

    scheduling incremental load

--- PipelineScheduler.py
+++ PipelineScheduler.py
@@ -1,34 +1,14 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-import subprocess
-import FetchData
-import os
-import sys
-from dotenv import load_dotenv
+import schedule
+import time
+import IncrementalLoad
 
-load_dotenv()
+def job():
+    IncrementalLoad.main()
+    print("Incremental load ran successfully")
 
-FMP_API_KEY = os.getenv("FMP_API_KEY")
-NEO4J_URI = os.getenv("NEO4J_URI")
-NEO4J_USER = os.getenv("NEO4J_USER")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+#schedule.every().day.at("06:00").do(job)  # Schedule to run daily at 6:00 AM
+schedule.every(5).minutes.do(job) # Schedule to run every 15 seconds
 
-python_executable = sys.executable
-
-def run_incremental_load():
-    """Function to run IncrementalLoad.py"""
-    try:
-        print("Running Incremental Load...")
-        subprocess.run(["python", "IncrementalLoad.py"], check=True)
-        print("Incremental Load completed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred during Incremental Load: {e}")
-
-# Create the scheduler
-scheduler = BlockingScheduler()
-scheduler.add_job(run_incremental_load, 'interval', seconds=1)
-
-print("Scheduler started. Incremental Load will run every 5 minutes.")
-try:
-    scheduler.start()
-except (KeyboardInterrupt, SystemExit):
-    print("Scheduler stopped.")
+while True:
+    schedule.run_pending()
+    time.sleep(100)
